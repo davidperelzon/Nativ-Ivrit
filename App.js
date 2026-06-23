976cd3281400b3af9517512ede7ce6bef052ee25
David Perelzon <david.araujo@ort.org.br>
2026-06-23 12:54:23 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import NavegacaoAbas from './src/navigation/NavegacaoAbas';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <NavegacaoAbas />
+    </NavigationContainer>
+  );
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Nativ-Ivrit
- Trabalho de Conclusão apresentado ao Curso Técnico em Informática da Escola ORT, como requisito parcial para obtenção do grau de Técnico em Informática.
+# Nativ Ivrit React Native\nProjeto base com navegação, listas e jogo simples.
