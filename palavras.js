b70d08f009343b6bf869b20a1863af9f468dc3dd
David Perelzon <david.araujo@ort.org.br>
2026-09-13 11:56:31 -0300

    Delete palavras.js

--- palavras.js
+++
@@ -1,6 +0,0 @@
-export default [
-{id:'1',hebraico:'שלום',portugues:'Olá'},
-{id:'2',hebraico:'מים',portugues:'Água'},
-{id:'3',hebraico:'בית',portugues:'Casa'},
-{id:'4',hebraico:'ספר',portugues:'Livro'}
-];
